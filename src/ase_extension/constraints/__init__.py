8132c56a6f4803d611550d087a905afc593c8494
mjhong0708 <mjhong0708@naver.com>
2025-05-19 18:06:22 +0900

    Initial commit

---
+++ src/ase_extension/constraints/__init__.py
@@ -0,0 +1,23 @@
+from ase import constraints
+
+from .fix import FixBondLengths
+
+__all__ = [
+    "FixBondLengths",
+]
+
+
+def dict2constraint(dct):
+    name = dct["name"]
+    kwargs = dct["kwargs"]
+    # First search in the current module (it takes precedence)
+    if name in __all__:
+        return globals()[name](**kwargs)
+    # Then search in the ase.constraints module
+    if name in constraints.__all__:
+        return getattr(constraints, name)(**kwargs)
+    raise ValueError(f"Unknown constraint name: {name}. Available: {__all__ + constraints.__all__}")
+
+
+# Fix for trajectory IO
+constraints.dict2constraint = dict2constraint
